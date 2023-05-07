b38ab74a702776ed14ad152922bd507dc81a11ad
myxsan <yksl3461@gmail.com>
2023-05-08 02:28:06 +0300

    basic login and register functionality

---
+++ client/src/components/login/index.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { usePostLoginMutation, usePostSignUpMutation } from "../../state/api";
+import { Button } from "react-chat-engine-advanced";
+
+const Login = ({ setUser, setSecret }) => {
+  const [isRegister, setIsRegister] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [triggerLogin, resultLogin] = usePostLoginMutation();
+  const [triggerSignup] = usePostSignUpMutation();
+
+  const handleLogin = () => {
+    triggerLogin({ username, password });
+  };
+
+  const handleRegister = () => {
+    triggerSignup({ username, password });
+  };
+
+  useEffect(() => {
+    if (resultLogin.data?.response) {
+      setUser(username);
+      setSecret(password);
+    }
+  }, [resultLogin.data]); // eslint-disable-line
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h2 className="title"> Chat App </h2>
+        <p
+          className="register-change"
+          onClick={() => setIsRegister(!isRegister)}
+        >
+          {isRegister ? "Already a user?" : "Are you a new user?"}
+        </p>
+
+        <div>
+          <input
+            type="text"
+            className="login-input"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            className="login-input"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="login-actions">
+          {isRegister ? (
+            <button type="button" onClick={handleRegister}>
+              Register
+            </button>
+          ) : (
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/state/api.js
+++ client/src/state/api.js
@@ -47,4 +47,6 @@ export const {
   usePostAiTextMutation,
   usePostAiCodeMutation,
   usePostAiAssistMutation,
+  usePostLoginMutation,
+  usePostSignUpMutation,
 } = api;
